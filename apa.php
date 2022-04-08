85d2ade94fd577968a5315c00ea2796bebf2a393
xmonsite <103287255+xmonsite@users.noreply.github.com>
2022-04-09 03:39:52 +0700

    Add files via upload

---
+++ apa.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width,minimum-scale=1,maximum-scale=1" name="viewport"> 
+  <meta name="description" content="Website"> 
+  <meta name="author" content="Website"> 
+  <meta name="keywords" content="Website"> 
+  <meta name="theme-color" content="#0B0B11" <!--="" essential="" meta="" tags="" --=""> 
+  <meta property="og:title" content="MONSITE"> 
+  <meta property="og:type" content="article"> 
+  <meta property="og:image" content="https://b.top4top.io/p_22897smfz2.jpg"> 
+  <meta property="og:url" content="https://monsite.guthub.io"> 
+  <meta name="twitter:card" content="summary_large_image"> 
+  <!--  Non-Essential, But Recommended --> 
+  <meta property="og:description" content="My own simple website"> 
+  <meta property="og:site_name" content="European Travel, Inc."> 
+  <meta name="twitter:image:alt" content="Alt text for image"> 
+  <!--  Non-Essential, But Required for Analytics --> 
+  <meta property="fb:app_id" content="your_app_id"> 
+  <meta name="twitter:site" content="@website-username"> 
+  <link rel="shortcut icon" href="https://image.similarpng.com/very-thumbnail/2021/09/Chrome-logo-design-on-transparent-background-PNG.png"> 
+  <!-- Title Page--> 
+  <title>MONSITE</title> 
+  
+  <!-- HTML -->
+  
+
+  <!-- Custom Styles -->
+  <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+  <h1>MONSITE</h1>
+ <img src="https://www.icegif.com/wp-content/uploads/icegif-87.gif" alt="IMG256">
+ <script>
+      var nama = prompt("Siapa nama kamu?", "");
+        document.write("<p>Hello "+ nama   +", selamat datang<br> dan dengarkan lagu ini!</p>");
+    </script>
+ <br>
+<video controls="" controlslist="nodownload" preload="none" width="100%" poster="https://pokemongohub.net/wp-content/uploads/2020/08/megaevolution-launch.jpg">
+            <source src="asset/432.mp4" type="video/mp4">
+        </video>
+        <marquee loop="2" scrollamount="16">numpamg lewat om...</marquee>
+  <!-- Project -->
+  <script src="main.js"></script>
+</body>
+</html>
+<article>
+            <?php 
+            include ("counter.php");
+            echo "<p style='color:red; font-weight:enchant_broker_list_dicts(broker)'> $kunjungan[0]× Di kunjungi</p>";
+            ?>
+   </article>
+           
